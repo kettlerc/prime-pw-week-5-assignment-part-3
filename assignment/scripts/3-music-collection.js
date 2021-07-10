6fd7c17754a10da448497410f2bcc41ef90471f5
Curtis Kettler <curtiskettler@gmail.com>
2021-07-10 14:59:28 -0500

    showCollection function created and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,10 +14,7 @@ function addToCollection(title, artist, yearPublished) {
   return album;
 } //end addToCollection
 
-//testing addToCollection function
-console.log(addToCollection('Album Name', 'Album Artist', '1999'));
-console.log(collection);
-//further testing
+//testing addToCollection
 addToCollection('The White Album', 'The Beatles', '1968');
 addToCollection('The Miseducation of Lauryn Hill', 'Lauryn Hill', '1998');
 addToCollection('In The Wee Small Hours', 'Frank Sinatra', '1955');
@@ -25,3 +22,14 @@ addToCollection('Crash', 'Dave Matthews Band', '1996');
 addToCollection('License to Ill', 'Beastie Boys', '1986');
 addToCollection('Monk\'s Dream', 'The Thelonius Monk Quartet', '1963');
 console.log(collection);
+
+//showCollection function
+function showCollection (array) {
+  console.log(array.length);
+  for (let albums of collection) {
+    console.log(albums.title + ' by ' + albums.artist + ', published in ' + albums.yearPublished);
+  }
+}// end showCollection
+
+//testing showCollection
+showCollection(collection);
